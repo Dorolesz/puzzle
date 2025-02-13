1e833e05940cdbbc2fe32a80a32de7b3d210acde
Dorina Laura Nagy <nagyneti2003@gmail.com>
2025-02-13 22:53:53 +0100

    swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -19,7 +20,16 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('Video streaming site')
+    .setDescription('The Api docs for the streaming site')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apidoc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/puzzles/dto/update-puzzle.dto.ts
+++ src/puzzles/dto/update-puzzle.dto.ts
@@ -1,6 +1,8 @@
+import { PartialType } from '@nestjs/swagger';
 import { IsString, IsInt, IsOptional, IsBoolean } from 'class-validator';
+import { CreatePuzzleDto } from './create-puzzle.dto';
 
-export class UpdatePuzzleDto {
+export class UpdatePuzzleDto extends PartialType(CreatePuzzleDto) {
   @IsString()
   @IsOptional()
   url?: string;
